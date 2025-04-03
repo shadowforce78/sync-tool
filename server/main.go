552c6abcb960ae8ff4d97c92daa2aaf81a4ce634
shadowforce78 <planque.adam@gmail.com>
2025-04-03 14:57:20 +0200

    refactor server main function to use a variable for the port and improve logging message

--- server/main.go
+++ server/main.go
@@ -15,11 +15,14 @@ func main() {
 		os.Mkdir(config.DataDir, 0755)
 	}
 
+	// Variable port pour le serveur
+	PORT := "8080"
+
 	// Enregistre les routes
 	http.HandleFunc("/upload", handlers.UploadHandler)
 	http.HandleFunc("/download", handlers.DownloadHandler)
-    http.HandleFunc("/list", handlers.ListHandler)
+	http.HandleFunc("/list", handlers.ListHandler)
 
-	fmt.Println("Serveur démarré sur http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Serveur démarré sur http://localhost:" + PORT)
+	http.ListenAndServe(":"+PORT, nil)
 }
